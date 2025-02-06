5e0fcdc845a56409ccf16fa5c3453bbb135d9616
Revathi Sekar <revathisekar2017@gmail.com>
2025-02-06 00:41:27 -0500

    Added index page to show the llm response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule,HttpClientModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'llm-app';
+  prompt: string = '';
+  response: string | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  generateResponse() {
+    const body = { prompt: this.prompt, model: 'mistral' };
+    this.http.post<{ response: string }>('http://localhost:4000/generate', body)
+      .subscribe({
+        next : (res : any)=>{
+          this.response = res.local_response
+          console.log(res);
+        },
+        error : (error : any)=>{
+          this.response = 'Error fetching response.'
+        }
+      }
+      );
+  }
 }
